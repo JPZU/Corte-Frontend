85b55f080823600fc726289be192b2858ac129de
JPZU <juanpablozp@icloud.com>
2025-06-24 15:55:51 -0500

    Updated ClothService to delete some methods

--- src/services/ClothService.ts
+++ src/services/ClothService.ts
@@ -30,38 +30,13 @@ export async function getClothByName(name: string) {
   return response.data;
 }
 
-export async function getIsActive() {
-  const response = await api.get("/cloths/is-active");
-  return response.data;
-}
-
-export async function getIsNotActive() {
-  const response = await api.get("/cloths/is-not-active");
-  return response.data;
-}
-
-export async function getClothBySupplier(supplierId: string) {
-  const response = await api.get(`/cloths/supplier/${supplierId}`);
-  return response.data;
-}
-
 export async function getClothByCategory(categoryId: number) {
   const response = await api.get(`/cloths/category/${categoryId}`);
   return response.data;
 }
 
-export async function getClothByUser(userId: number) {
-  const response = await api.get(`/cloths/user/${userId}`);
-  return response.data;
-}
-
 export async function getByCreatedAtBetween(data: any) {
   const response = await api.get("/cloths/created-between", data);
-  return response.data;
-}
-
-export async function getBySupplierInvoice(supplierInvoice: string) {
-  const response = await api.get(`/cloths/supplier-invoice/${supplierInvoice}`);
   return response.data;
 }
 
@@ -76,11 +51,8 @@ export async function filterCloths(params: {
   page: number;
   size: number;
   name?: string;
-  supplierInvoice?: string;
-  userId?: number;
   isActive?: boolean;
   categoryId?: number;
-  supplierId?: string;
 }) {
   const response = await api.get("/cloths/filter", { params });
   return response.data;
